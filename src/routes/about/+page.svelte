42534ef8faf87025c4abcb0c13f75f5634506ed2
GiftTeddy <serathi02@gmail.com>
2024-10-22 15:39:46 +0200

    about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,52 +4,65 @@
 </svelte:head>
 
 <!-- Main container -->
-<div class="min-h-screen py-16 bg-#21409A">
+<div class="min-h-screen py-24 bg-#21409A">
 	<!-- Main Title -->
-	<h1 class="text-6xl font-bold text-white text-center mb-12">ABOUT US PAGES</h1>
+	<h1 class="text-6xl font-bold text-white text-center mb-12 ">ABOUT US </h1>
   
 	<!-- Main Grid Container -->
 	<div class="grid grid-cols-1 md:grid-cols-2 gap-8 px-10">
-  
+		<!-- flex-1 grid grid-cols-1 md:grid-cols-1 -->
+
 	  <!-- First Card (Chicken) -->
 	  <div class="relative bg-white rounded-tl-[100px] rounded-br-[100px] overflow-hidden shadow-lg">
 		<img
-		  src="/path/to/chicken-image.jpg"
-		  alt="Chicken"
+		  src=""
+		  alt=""
 		  class="w-full h-64 object-cover"
 		/>
 		<div class="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-white text-center p-4">
-		  <h2 class="text-xl font-bold mb-2">Village Tech</h2>
-		  <p class="mb-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem iure, facere beatae voluptates quis eaque voluptatum repellat magnam unde earum aperiam magni reiciendis! Reprehenderit ut quibusdam est sint, voluptatem magnam..</p>
-		  <a href="#" class="underline hover:text-yellow-300">Learn more</a>
+		  <!-- <h2 class="text-xl font-bold mb-2">Village Tech</h2> -->
+		  <p class="mb-2">Village Tech Training Solutions is a registered training and skills development service provider which was founded in 2015 with the main aim of assisting our society to address the high unemployment problem and skills shortage especially amongst the youth, rural societies, women and persons living with disabilities.</p>
+		  <!-- <a href="#" class="underline hover:text-yellow-300">Learn more</a> -->
 		</div>
 	  </div>
   
 	  <!-- Second Card (Mavi Jeans) -->
 	  <div class="relative bg-white rounded-tl-[100px] rounded-br-[100px] overflow-hidden shadow-lg flex flex-col items-center">
 		<img
-		  src="/path/to/mavi-jeans-image.jpg"
-		  alt="Mavi Jeans"
+		  src=""
+		  alt=""
 		  class="w-full h-64 object-cover"
 		/>
 		<div class="absolute inset-0 flex flex-col justify-center items-center bg-white bg-opacity-90 text-center p-4">
 		  <h2 class="text-xl font-bold mb-4 text-black">Village Tech</h2>
 		  <p class="text-gray-800 mb-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus aspernatur necessitatibus, numquam quas cupiditate ducimus distinctio, quae ipsam, corporis quod repellendus nobis. Eos ullam officia laborum veritatis, culpa vel voluptate?</p>
-		  <a href="#" class="text-blue-600 underline hover:text-blue-800">Learn more</a>
+		  <!-- <a href="#" class="text-blue-600 underline hover:text-blue-800">Learn more</a> -->
 		</div>
 	  </div>
   
 	  <!-- Third Card (Future Kind) -->
 	  <div class="relative bg-white rounded-tl-[100px] rounded-br-[100px] overflow-hidden shadow-lg">
 		<img
-		  src="/path/to/future-kind-image.jpg"
-		  alt="Future Kind"
+		  src=""
+		  alt=""
 		  class="w-full h-64 object-cover"
 		/>
 		<div class="absolute inset-0 flex flex-col justify-center items-center bg-white bg-opacity-90 text-center p-4">
 		  <h2 class="text-xl font-bold mb-4 text-black">Village Tech</h2>
 		  <p class="text-gray-800 mb-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error repudiandae laudantium rem consequuntur vero exercitationem. Id, ducimus dolorum incidunt aliquid at enim? Reprehenderit minus similique ab dignissimos ipsum explicabo ipsam?</p>
-		  <a href="#" class="text-blue-600 underline hover:text-blue-800">Learn more</a>
+		  <!-- <a href="#" class="text-blue-600 underline hover:text-blue-800">Learn more</a> -->
+		</div>
+	  </div>
+	  <div class="relative bg-white rounded-tl-[100px] rounded-br-[100px] overflow-hidden shadow-lg flex flex-col items-center">
+		<img
+		  src=""
+		  alt=""
+		  class="w-full h-64 object-cover"
+		/>
+		<div class="absolute inset-0 flex flex-col justify-center items-center bg-white bg-opacity-90 text-center p-4">
+		  <h2 class="text-xl font-bold mb-4 text-black">Village Tech</h2>
+		  <p class="text-gray-800 mb-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus aspernatur necessitatibus, numquam quas cupiditate ducimus distinctio, quae ipsam, corporis quod repellendus nobis. Eos ullam officia laborum veritatis, culpa vel voluptate?</p>
+		  <!-- <a href="#" class="text-blue-600 underline hover:text-blue-800">Learn more</a> -->
 		</div>
 	  </div>
   
