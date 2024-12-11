f66a517c4c748635e42937a6f005ddec1584eefd
Ristar-art <mochochokoboiketlo@gmail.com>
2024-12-11 17:59:40 +0200

    fix: added netifly adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter(
+			
+		),
 		// Add the prerender configuration for handleHttpError
 		prerender: {
 			handleHttpError: ({ status, path, referrer, message }) => {
